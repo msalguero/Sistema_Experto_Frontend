44a5cdb84b1e4ec26e18d33dd1133adba6dd39f7
Dennis Molina <dennismolina.17@gmail.com>
2016-12-05 09:04:05 -0600

    fixed typo

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@
     '$urlRouterProvider',
     function($stateProvider, LoopBackResourceProvider,$urlRouterProvider) {
 
-      LoopBackResourceProvider.setUrlBase('http://0.0.0.0:3000/api');
+      LoopBackResourceProvider.setUrlBase('https://rubric-expert.herokuapp.com/api');
 
 
       var loginState = {
