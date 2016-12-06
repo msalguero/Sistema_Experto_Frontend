f232662cc8b6529306f0ac7ce6e82880ef9de93b
Dennis Molina <dennismolina.17@gmail.com>
2016-12-05 22:00:56 -0600

    fixed URL for Poll emails

--- app/main.js
+++ app/main.js
@@ -242,45 +242,6 @@
 
   angular.module('InvestigationApp')
 
-  .controller('Layout', [
-    '$rootScope', '$scope', '$state', 'Account',
-    function($rootScope, $scope, $state, Account) {
-      var ctrl = this;
-      
-      $scope.logout = function(){
-        Account.logout(
-          function() {
-            $state.go("login");
-          }, function(res) {
-            $scope.error = true;
-          });
-      };
-
-      $scope.investigations = function(){
-        $state.go("main.investigations");
-      };
-
-      $scope.init = function(){
-        Account.getCurrent(
-          function(response) {
-              var user = response;
-              $scope.userName = user.username;
-            }, function(res) {
-              $state.go("login");
-            });
-      }
-
-      $scope.init();
-    }
-  ]);
-
-}());
-(function () {
-
-'use strict';
-
-  angular.module('InvestigationApp')
-
   .controller('PollList', [
     '$scope', '$state', 'Poll',
     function($scope, $state, Poll) {
@@ -312,6 +273,45 @@
               }); 
           });
       };*/
+    }
+  ]);
+
+}());
+(function () {
+
+'use strict';
+
+  angular.module('InvestigationApp')
+
+  .controller('Layout', [
+    '$rootScope', '$scope', '$state', 'Account',
+    function($rootScope, $scope, $state, Account) {
+      var ctrl = this;
+      
+      $scope.logout = function(){
+        Account.logout(
+          function() {
+            $state.go("login");
+          }, function(res) {
+            $scope.error = true;
+          });
+      };
+
+      $scope.investigations = function(){
+        $state.go("main.investigations");
+      };
+
+      $scope.init = function(){
+        Account.getCurrent(
+          function(response) {
+              var user = response;
+              $scope.userName = user.username;
+            }, function(res) {
+              $state.go("login");
+            });
+      }
+
+      $scope.init();
     }
   ]);
 
