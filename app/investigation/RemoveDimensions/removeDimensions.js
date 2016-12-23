590dcf17d691e6912dfeb06d613c09fd340507b1
Dennis Molina <dennismolina.17@gmail.com>
2016-12-23 14:34:54 -0600

    dimensions now appear checked

--- app/investigation/RemoveDimensions/removeDimensions.js
+++ app/investigation/RemoveDimensions/removeDimensions.js
@@ -12,15 +12,24 @@
         id: $stateParams.id,
         filter: {include:  ['variables']}
       }, function(investigation){
-        $scope.variables = investigation.variables;
-        console.log("$scope.variables ", $scope.variables);
+        
+        for(var i = 0; i<$scope.investigation.variables.length; i++){
+          for(var x = 0; x<$scope.investigation.variables[i].dimensions.length; x++){
+            if($scope.investigation.variables[i].dimensions[x].votes != 0){
+              $scope.investigation.variables[i].dimensions[x].checked = true;
+            }
+          }
+        }
+        console.log("$scope.variables ", $scope.investigation.variables);
       });
 
       $scope.submit = function(){
+
+
         
         // Investigation.prototype$updateAttributes(
         //    {id:    $stateParams.id},
-        //    {step: 3},
+        //    {step: 5},
         //    function(){
         //     $state.go('main.viewInvestigation',{id: $stateParams.id} );
         //    }
