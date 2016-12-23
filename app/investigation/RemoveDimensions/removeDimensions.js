3cf1df045611ffe30d4f303fcc0520a3d49643eb
Dennis Molina <dennismolina.17@gmail.com>
2016-12-23 15:05:47 -0600

    update dimensions on variables and update investigation to step 5

--- app/investigation/RemoveDimensions/removeDimensions.js
+++ app/investigation/RemoveDimensions/removeDimensions.js
@@ -23,17 +23,41 @@
         console.log("$scope.variables ", $scope.investigation.variables);
       });
 
+      var updateVariable = function(variableId, newDimensions){
+        Variable.prototype$updateAttributes(
+           {id:    variableId},
+           {dimensions: newDimensions},
+           function(err, cb){
+            console.log("ok: ", cb);
+            console.log("error: ", err);
+            // $state.go('main.viewInvestigation',{id: $stateParams.id} );
+           }
+        );
+      }
+
       $scope.submit = function(){
 
+        for(var i = 0; i<$scope.investigation.variables.length; i++){
+          var newDimensions = []
+          for(var x = 0; x<$scope.investigation.variables[i].dimensions.length; x++){
 
+            if($scope.investigation.variables[i].dimensions[x].checked){
+              // $scope.investigation.variables[i].dimensions = $scope.investigation.variables[i].dimensions.splice(x,1);
+              newDimensions.push($scope.investigation.variables[i].dimensions[x])
+            }
+          }
+          //$scope.investigation.variables[i].dimensions = newDimensions;
+          updateVariable($scope.investigation.variables[i].id, newDimensions);
+
+        }
         
-        // Investigation.prototype$updateAttributes(
-        //    {id:    $stateParams.id},
-        //    {step: 5},
-        //    function(){
-        //     $state.go('main.viewInvestigation',{id: $stateParams.id} );
-        //    }
-        // );
+        Investigation.prototype$updateAttributes(
+           {id:    $stateParams.id},
+           {step: 5},
+           function(){
+            $state.go('main.viewInvestigation',{id: $stateParams.id} );
+           }
+        );
         
       }
     }
