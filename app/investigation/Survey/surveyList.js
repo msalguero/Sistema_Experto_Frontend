e45effc7a5e4dcca4ce2294b24365a4fbf8265f7
Dennis Molina <dennismolina.17@gmail.com>
2016-12-27 21:39:44 -0600

    added style to surveys

--- app/investigation/Survey/surveyList.js
+++ app/investigation/Survey/surveyList.js
@@ -7,7 +7,36 @@
   .controller('SurveyList', [
     '$scope', '$state', 'Investigation', '$mdDialog', 'Account','$stateParams' ,
     function($scope, $state, Investigation, $mdDialog, Account, $stateParams) {
-      
+
+      $scope.setStyle = function(poll){
+        var myObj = {
+          "box-shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          "margin": "1%",
+          "border-radius": "5px",
+          "overflow": "hidden",
+          "width": "30%"
+
+        };
+        switch(poll.type){
+          case "1":
+            myObj["background-color"] = "white";
+          break;
+          case "2":
+            myObj["background-color"] = "white";
+            break;
+          case "3":
+            myObj["background-color"] = "white";
+            break;
+        }
+
+        return myObj;
+
+      }
+
+
+
+
+
       $scope.investigation = Investigation.findById({
         id: $stateParams.id,
         filter: {include:  ['polls']}
